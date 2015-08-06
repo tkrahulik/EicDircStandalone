e39bb47045fb46c2b5f884172eaa770d3226d736
tkrahulik <thomas.krahulik@stonybrook.edu>
2015-08-06 15:09:24 -0400

    Remove build directory from GitHub Repository

--- EicDircStandalone-build/CMakeFiles/N06.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/N06"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/N06.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
