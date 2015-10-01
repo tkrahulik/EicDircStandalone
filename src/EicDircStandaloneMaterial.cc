39a1ba2ca968a1f65c4d65f29c5067f0563301d0
Nils Feege <nils.feege@stonybrook.edu>
2015-10-01 15:55:51 -0400

    Update index of refraction of fused silica (DIRC quartz) with wavelength dependent numbers from BaBar reference

--- src/EicDircStandaloneMaterial.cc
+++ src/EicDircStandaloneMaterial.cc
@@ -106,28 +106,42 @@ EicDircStandaloneMaterial::EicDircStandaloneMaterial(){
   Quartz->AddElement(Si, natoms=1);
   Quartz->AddElement(O, natoms=2);
 
-  const G4int q_nEntries = 32;
+  const G4int q_nEntries =44;
 
-  G4double q_PhotonEnergy[q_nEntries] = 
-    { 2.034*eV, 2.068*eV, 2.103*eV, 2.139*eV,
-      2.177*eV, 2.216*eV, 2.256*eV, 2.298*eV,
-      2.341*eV, 2.386*eV, 2.433*eV, 2.481*eV,
-      2.532*eV, 2.585*eV, 2.640*eV, 2.697*eV,
-      2.757*eV, 2.820*eV, 2.885*eV, 2.954*eV,
-      3.026*eV, 3.102*eV, 3.181*eV, 3.265*eV,
-      3.353*eV, 3.446*eV, 3.545*eV, 3.649*eV,
-      3.760*eV, 3.877*eV, 4.002*eV, 4.136*eV
+  /* PhotonEnergy and RefractiveIndex for BaBar Fused Silica (quartz) from
+   * J. Cohen-Tanugi et al, 'Optical Properties of the DIRC Fused Silica Cherenkov
+   * Radiator', SLAC-PUB-9735, April 2003, arXiv:hep-ex/0305001v1
+   */
+  G4double q_PhotonEnergy[q_nEntries] =
+    { 2.09628*eV, 2.12869*eV, 2.16212*eV, 2.19456*eV,
+      2.23224*eV, 2.26903*eV, 2.30478*eV, 2.34402*eV,
+      2.38462*eV, 2.42413*eV, 2.46758*eV, 2.51531*eV,
+      2.55653*eV, 2.6078*eV,  2.65513*eV, 2.70735*eV,
+      2.76165*eV, 2.81479*eV, 2.87531*eV, 2.93848*eV,
+      2.99872*eV, 3.06549*eV, 3.1353*eV,  3.20397*eV,
+      3.28031*eV, 3.35556*eV, 3.44444*eV, 3.53283*eV,
+      3.62026*eV, 3.71803*eV, 3.82122*eV, 3.92372*eV,
+      4.03883*eV, 4.16089*eV, 4.29056*eV, 4.42857*eV,
+      4.57576*eV, 4.72351*eV, 4.89133*eV, 5.06054*eV,
+      5.25365*eV, 5.47485*eV, 5.68773*eV, 5.93283*eV
     };
 
+  /* PhotonEnergy and RefractiveIndex for BaBar Fused Silica (quartz) from
+   * J. Cohen-Tanugi et al, 'Optical Properties of the DIRC Fused Silica Cherenkov
+   * Radiator', SLAC-PUB-9735, April 2003, arXiv:hep-ex/0305001v1
+   */
   G4double q_RefractiveIndex[q_nEntries] = 
-    { 1.457, 1.457, 1.457, 1.457,
-      1.457, 1.457, 1.457, 1.457,
-      1.457, 1.457, 1.457, 1.457,
-      1.457, 1.457, 1.457, 1.457,
-      1.457, 1.457, 1.457, 1.457,
-      1.457, 1.457, 1.457, 1.457,
-      1.457, 1.457, 1.457, 1.457,
-      1.457, 1.457, 1.457, 1.457
+    { 1.45728, 1.45730, 1.45732, 1.45735,
+      1.45838, 1.45840, 1.45842, 1.45945,
+      1.45948, 1.45950, 1.46053, 1.46055,
+      1.46057, 1.46160, 1.46163, 1.46266,
+      1.46268, 1.46371, 1.46524, 1.46527,
+      1.46680, 1.46783, 1.46785, 1.46888,
+      1.46991, 1.47094, 1.47198, 1.47401,
+      1.47605, 1.47708, 1.47811, 1.48015,
+      1.48219, 1.48524, 1.48728, 1.49133,
+      1.49438, 1.49843, 1.50249, 1.50755,
+      1.51362, 1.51969, 1.52778, 1.53889,
     };
 
   G4MaterialPropertiesTable* fMPT_q = new G4MaterialPropertiesTable();
